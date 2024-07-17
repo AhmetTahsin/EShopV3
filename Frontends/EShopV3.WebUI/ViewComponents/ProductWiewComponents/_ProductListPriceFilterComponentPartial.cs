602e58d10bf97fc97985130d8a45cd0404ef4e16
AhmetTahsin <ahmet.tahsin.cam@gmail.com>
2024-07-18 01:10:01 +0300

    UI işlemleri Devam Ediyor ProductList işlemleri başladı

---
+++ Frontends/EShopV3.WebUI/ViewComponents/ProductWiewComponents/_ProductListPriceFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShopV3.WebUI.ViewComponents.ProductWiewComponents
+{
+    public class _ProductListPriceFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Inboke()
+        {
+            return View();
+        }
+    }
+}
